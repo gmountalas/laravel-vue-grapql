28b0fd4099e3b122efed1b01c11ed811e282ed96
gmountalas <mountalasgeorge@gmail.com>
2021-08-05 23:47:14 +0300

    41. Vue Apollo Smart Queries

--- resources/js/PostList.vue
+++ resources/js/PostList.vue
@@ -3,3 +3,21 @@
         PostList
     </div>
 </template>
+
+<script>
+import gql from "graphql-tag";
+
+export default {
+    apollo: {
+        // Tagged template
+        posts: gql`
+            {
+                posts {
+                    id
+                    title
+                }
+            }
+        `
+    }
+};
+</script>
