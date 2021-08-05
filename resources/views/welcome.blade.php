8c15a427caec107f098e8a04c8b96af65ba11df9
gmountalas <mountalasgeorge@gmail.com>
2021-08-05 22:20:18 +0300

    38. Setting Up Routes in Vue Router

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,15 +9,8 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="antialiased">
-        <div id="app"></div>
-        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-            <div class="flex-shrink-0">
-                <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
-            </div>
-            <div>
-                <div class="text-xl font-medium text-black">ChitChat</div>
-                <p class="text-gray-500">You have a new message!</p>
-            </div>
+        <div id="app">
+            <router-view></router-view>
         </div>
     </body>
 </html>
