db2b5b42e8876c0651a500b3e10ad02d2f47831a
gmountalas <mountalasgeorge@gmail.com>
2021-08-08 13:59:52 +0300

    46. Post Page Markup & CSS Flexbox

--- public/js/app.js
+++ public/js/app.js
@@ -28952,9 +28952,57 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", [_vm._v("\n    Post 1\n")])
+  return _vm._m(0)
 }
-var staticRenderFns = []
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c(
+      "div",
+      { staticClass: "container mx-auto px-4b w-full md:w-3/4 lg:w-1/2 mt-20" },
+      [
+        _c("div", [
+          _c("div", { staticClass: "text-lg text-gray-600" }, [
+            _vm._v("\n            By me in Links * 3 hours ago\n        ")
+          ]),
+          _vm._v(" "),
+          _c("h1", { staticClass: "text-5xl mt-10 font-bold mb-12" }, [
+            _vm._v("The post title")
+          ]),
+          _vm._v(" "),
+          _c(
+            "p",
+            { staticClass: "text-gray-700 pb-3 mb-12 whitespace-pre-line" },
+            [_vm._v("\n            The blog post content\n        ")]
+          ),
+          _vm._v(" "),
+          _c("div", { staticClass: " mb-24 flex" }, [
+            _c("div", { staticClass: " mr-6" }, [
+              _c("img", {
+                staticClass: " w-16 h-16 rounded-full",
+                attrs: { src: "/storage/faces/ahmad.jpg", alt: "Author Avatar" }
+              })
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: " flex flex-col justify-center" }, [
+              _c("div", { staticClass: " text-xl text-gray-600" }, [
+                _vm._v("Written by me")
+              ]),
+              _vm._v(" "),
+              _c("div", { staticClass: " text-gray-600" }, [
+                _vm._v(
+                  "\n                    Published in Links on August 8, 2021\n                "
+                )
+              ])
+            ])
+          ])
+        ])
+      ]
+    )
+  }
+]
 render._withStripped = true
 
 
